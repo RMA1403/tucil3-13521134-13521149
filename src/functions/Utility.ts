5c2e3797651565347ea3b27760147a7c583380ea
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2023-04-10 20:16:24 +0700

    dev: astar

--- src/functions/Utility.ts
+++ src/functions/Utility.ts
@@ -1,4 +1,5 @@
-import Graph from "../classes/Graph";
+import { readFile } from "fs/promises";
+import Graph, { coordinate } from "../classes/Graph";
 
 function fileReader(fileData: string): Graph {
   const inputFile: string[] = fileData.split("\n");
@@ -13,10 +14,26 @@ function fileReader(fileData: string): Graph {
 
   for (let i = graph.getVertexCount() + 2; i < inputFile.length; i++) {
     const temp: string[] = inputFile[i].split(" ");
-    graph.addEdge(Number(temp[0]), Number(temp[1]), 10, temp[2]);
+    graph.addEdge(Number(temp[0]), Number(temp[1]), Number(temp[2]), temp[3]);
   }
 
   return graph;
 }
 
-export { fileReader };
+function euclideanDistance(c1: coordinate, c2: coordinate) {
+  return Math.sqrt((c2.lat - c1.lat) ** 2 + (c2.long - c1.long) ** 2);
+}
+
+function enqueuePQ(pq: Object[], x: Object): void {
+  let idx = 0;
+  while (idx < pq.length && pq[idx] < x) idx++;
+  pq.splice(idx, 0, x);
+}
+
+function dequeuePQ(pq: Object[]): Object {
+  const x = pq[0];
+  pq.splice(0, 1);
+  return x;
+}
+
+export { fileReader, euclideanDistance, enqueuePQ, dequeuePQ };
